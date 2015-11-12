116cbcd0071399bfe8ab4117acacd55b6d01a3c7
EKR <ekr@rtfm.com>
2015-11-11 21:12:10 -0800

    Use config file

--- run.js
+++ run.js
@@ -18,7 +18,7 @@ function assert(condition, message) {
 }
 
 function debug(msg) {
-    console.log(msg);
+//    console.log(msg);
 }
 function recordVersions(vlist) {
     debug(vlist);
@@ -33,28 +33,32 @@ function recordVersions(vlist) {
 }
 
 
-function lookForBreaks(channel, metric, bucket, series) {
+function lookForBreaks(channel, metric, buckets, series) {
     var baseline=[];
-    series.map(function(hist, i, date) {
-        var sd;
-        var mean;
-        var v;
-        var value = hist.values[bucket]/hist.count;
-        debug(date);
-        if (baseline.length > 5) {
-            v = gauss.Vector(baseline);
-            mean = v.mean();
-            sd = v.stdev();
-            if (Math.abs(value - mean) > (2 * sd)) {
-                console.log("ANOMALY: " + channel + ":" + metric + " " + date + " mean = " + mean
-                            + " value = " + value);
+
+    buckets.forEach(function(bucket) {
+        debug("Looking for breaks in bucket " + bucket);
+        series.map(function(hist, i, date) {
+            var sd;
+            var mean;
+            var v;
+            var value = hist.values[bucket]/hist.count;
+            debug(date);
+            if (baseline.length > 5) {
+                v = gauss.Vector(baseline);
+                mean = v.mean();
+                sd = v.stdev();
+                if (Math.abs(value - mean) > (2 * sd)) {
+                    console.log("ANOMALY: " + metric + ":" + channel + ":" + bucket + " " + date + " mean=" + mean
+                                + " value=" + value);
+                }
             }
-        }
-        baseline.push(value);
+            baseline.push(value);
+        });
     });
 }
 
-function timeSeries(channel, metric, bucket, lookback) {
+function timeSeries(channel, metric, buckets, lookback) {
     var version_ct = Math.floor((lookback / 42) + 2);  // Max number of versions to capture lookback days
     var i;
     var v;
@@ -76,7 +80,7 @@ function timeSeries(channel, metric, bucket, lookback) {
             }
             version_ct--;
             if (!version_ct) {
-                lookForBreaks(channel, metric, bucket, evolutions.dateRange(dates[0], dates[1]));
+                lookForBreaks(channel, metric, buckets, evolutions.dateRange(dates[0], dates[1]));
             }
         });
     }
@@ -98,8 +102,8 @@ Telemetry.init(function() {
     for (metric in config) {
         cconfig = config[metric];
         cconfig.channels.forEach(function(channel) {
-            debug("Measuring " + metric + " channel=" + channel);
-            timeSeries(channel, metric, 5, 60);
+            debug("Measuring " + metric + " channel=" + channel + "buckets =" + cconfig.buckets );
+            timeSeries(channel, metric, cconfig.buckets, 60);
         });
     }
 });
